3a996fb18cad542045ca715161d9945b2994fdf5
Brendon Costa <brendoncosta93@gmail.com>
2019-02-21 11:38:34 -0300

    improving EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { Entry } from "../shared/entry.model"
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 import { throwError, Observable } from 'rxjs';
 
 @Injectable({
@@ -16,23 +16,33 @@ export class EntryService extends BaseResourceService<Entry>{
   }
 
   create(entry: Entry): Observable<Entry>{
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-
-        return super.create(entry);
-      })
-    )
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this))
   }
 
   update(entry: Entry): Observable<Entry>{
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this))
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
-
-        return super.update(entry);
-      })
-    )
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
+    );
   }
+
+  //Como o metodo era
+  // update(entry: Entry): Observable<Entry>{
+  //   return this.categoryService.getById(entry.categoryId).pipe(
+  //     flatMap(category => {
+  //       entry.category = category;
+
+  //       return super.update(entry);
+  //     })
+  //   )
+  // }
+  //end
 
 }
